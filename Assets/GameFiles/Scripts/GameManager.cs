ccf0b5de793c337ac87d9d9f87ee16e0b44c4fdd
Oleg <loshkin.oleg.95@gmail.com>
2019-12-23 14:01:09 +0100

    First commit

---
+++ Assets/GameFiles/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Bolt.Matchmaking;
+
+[BoltGlobalBehaviour]
+class GameManager : GlobalEventListener
+{
+    Dictionary<BoltConnection, BoltEntity> clientPlayers = new Dictionary<BoltConnection, BoltEntity>();
+    BoltEntity serverPlayer = null;
+
+    public override void BoltStartDone()
+    {
+        if (BoltNetwork.IsServer)
+        {
+            serverPlayer = BoltNetwork.Instantiate(BoltPrefabs.Player);
+            serverPlayer.TakeControl();
+        }
+    }
+
+    public override void Connected(BoltConnection connection)
+    {
+        base.Connected(connection);
+
+        if (BoltNetwork.IsServer)
+        {
+            clientPlayers.Add(connection, BoltNetwork.Instantiate(BoltPrefabs.Player));
+            clientPlayers[connection].AssignControl(connection);
+        }
+    }
+
+    public override void Disconnected(BoltConnection connection)
+    {
+        base.Disconnected(connection);
+
+        if (BoltNetwork.IsServer)
+        {
+            clientPlayers[connection].ReleaseControl();
+            clientPlayers[connection].DestroyDelayed(0.0f);
+            clientPlayers.Remove(connection);
+        }
+    }
+}
---
+++ Assets/GameFiles/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class PlayerController : EntityBehaviour<IPlayerState>
+{
+    Vector3 movementInput = Vector3.zero;
+    Vector2 mouseInput = Vector2.zero;
+
+    public override void Attached()
+    {
+        base.Attached();
+
+        state.SetTransforms(state.StateTransform, transform);
+    }
+
+    public override void SimulateController()
+    {
+        base.SimulateController();
+
+
+        IMovementCommandInput cmd = MovementCommand.Create();
+        cmd.Movement = movementInput;
+        cmd.Mouse = mouseInput;
+        entity.QueueInput(cmd);
+    }
+
+    public override void ExecuteCommand(Command command, bool resetState)
+    {
+        base.ExecuteCommand(command, resetState);
+
+        MovementCommand cmd = command as MovementCommand;
+
+        if (resetState) // Controller only.
+        {
+            transform.position = cmd.Result.Position;
+            transform.rotation = cmd.Result.Rotation;
+        }
+        else // Controller and owner.
+        {
+            transform.position = transform.position + cmd.Input.Movement * BoltNetwork.FrameDeltaTime;
+            cmd.Result.Position = transform.position;
+
+            transform.rotation *= Quaternion.Euler(cmd.Input.Mouse.y, cmd.Input.Mouse.x, 0);
+            cmd.Result.Rotation = transform.rotation;
+        }
+    }
+
+    private void Update()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.z = Input.GetAxisRaw("Vertical");
+        mouseInput.x = Input.GetAxisRaw("Mouse X");
+        mouseInput.y = Input.GetAxisRaw("Mouse Y");
+    }
+}
